ad6ed593dc22501995220e9a35aa4f234077fa66
Mhmd-abi-hachem <mohamadhachem908@gmail.com>
2025-12-22 16:36:58 +0200

    Update how-it-works section full ui

--- app/_components/sections/HowItWorks.js
+++ app/_components/sections/HowItWorks.js
@@ -9,11 +9,12 @@ import {
   faVideo,
 } from "@fortawesome/free-solid-svg-icons";
 import StepCard from "../StepCard";
-import Button from "../Button";
-import Link from "next/link";
+import StepsImage from "@/public/steps.webp";
+import Image from "next/image";
 
 const steps = [
   {
+    number: "1",
     title: "Create Your Profile",
     icon: (
       <FontAwesomeIcon icon={faUserPlus} className="w-5 h-5 text-primary-600" />
@@ -22,30 +23,35 @@ const steps = [
       "Sign up and tell us about your field of interest, career goals, and what kind of help you're looking for",
   },
   {
+    number: "2",
     title: "Find Your Mentor",
     icon: (
       <FontAwesomeIcon
         icon={faMagnifyingGlass}
-        className="w-5 h-5 text-cyan-600"
+        className="w-5 h-5 text-primary-600"
       />
     ),
     description:
       "Browse through verified professionals, filter by industry, skills, and session type to find the perfect match",
   },
   {
+    number: "3",
     title: "Book a Session",
     icon: (
       <FontAwesomeIcon
         icon={faCalendarDays}
-        className="w-5 h-5 text-purple-600"
+        className="w-5 h-5 text-primary-600"
       />
     ),
     description:
       "Choose an available time slot and session type that fits your needs—resume review, portfolio critique, or career advice",
   },
   {
+    number: "4",
     title: "Meet & Learn",
-    icon: <FontAwesomeIcon icon={faVideo} className="w-5 h-5 text-amber-600" />,
+    icon: (
+      <FontAwesomeIcon icon={faVideo} className="w-5 h-5 text-primary-600" />
+    ),
     description:
       "Join your video session, get personalized guidance, and take your career to the next level with expert advice",
   },
@@ -70,31 +76,29 @@ export default function HowItWorks() {
           </p>
         </div>
 
-        {/* List */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-16">
-          {steps.map((step, idx) => (
-            <StepCard
-              key={idx}
-              title={step.title}
-              icon={step.icon}
-              description={step.description}
-              idx={idx}
-            />
-          ))}
-        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 sm:gap-12 items-center justify-center">
+          {/* Steps list */}
+          <div className="flex flex-col gap-8 items-center">
+            {steps.map((step) => (
+              <div key={step.number} className="flex gap-8 items-start">
+                <StepCard
+                  title={step.title}
+                  icon={step.icon}
+                  description={step.description}
+                  number={step.number}
+                />
+              </div>
+            ))}
+          </div>
 
-        <div className="bg-gradient-to-br from-gray-900 to-indigo-900 rounded-3xl p-12 text-center">
-          <h3 className="text-3xl font-bold text-primary-100 mb-4">
-            Ready to Get Started?
-          </h3>
-          <p className="text-xl text-gray-300 mb-8">
-            Join thousands of students who are already getting the guidance they
-            need
-          </p>
-          <div className="flex justify-center">
-            <Button variation="tertiary" size="large" classes="w-auto!">
-              <Link href="/auth/signup">Create Your Free Account</Link>
-            </Button>
+          <div className="relative scale-90 aspect-square w-full rounded-2xl shadow-2xl">
+            <Image
+              src={StepsImage}
+              alt="A happy student using MentorMap"
+              fill
+              placeholder="blur"
+              className="object-cover rounded-2xl"
+            />
           </div>
         </div>
       </div>
